49b866c82f8c90ba0585eac637f7890609aee44f
root <Yabiti>
2023-02-16 08:36:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>
+/**
+ *  main -A program  that prints a line with puts functions
+ *  Return 0 (success)
+ */
+int main(void)
+{ 
+puts("\"programming is like building a multilingual puzzle");
+return (0)'
+}

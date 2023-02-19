c071bc2796ac70f0a26da24d68538cffbb3b1e46
root <yabetseyoannes@gmail.com>
2023-02-19 03:39:54 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - prints alphanin lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar (ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar (ch);
+
+	putchar(\"n)
+	return (0);
+}

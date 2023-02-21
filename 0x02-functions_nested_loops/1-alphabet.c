f7b9cdac71372c3544e9d5cd1a773b4e4fbc2206
root <yabetseyoannes@gmail.com>
2023-02-21 04:50:41 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - Entry
+ * description: prints _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str = _putchar";
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+

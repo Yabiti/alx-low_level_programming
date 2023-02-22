b601d151f3129be5612b623a3a92adde73f5062f
root <yabetseyoannes@gmail.com>
2023-02-22 08:28:10 -0800

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,16 @@
 
 
 /**
- * _islower - check for lower case letter 
+ * _islower - check for lower case letter
  * @c : character to check the case
  * Return: Always 0
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 	{
-	
 	return (1);
-	}
+
 	return (0);
 }

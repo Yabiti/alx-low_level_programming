c27961017a369c6806ba23e6bcb8b514582e7b9b
Yabiti <yab@gmail.com>
2023-06-15 19:18:29 +0300

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
-*malloc=checked memory allocates memory using malloc
+*malloc_checked memory allocates memory using malloc
 *@b: number of bytes to allocate
 *Return: a pointer to the allocated memory
 */
 void *malloc_checked(unsigned int b)
 {
-    void ptr;
+    void *ptr;
     ptr = malloc(b);
     if (ptr == NULL)
     exit(98);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,11 +6,11 @@
 #include <limits.h>
 
 int _putchar(char c);
-void *malloc_checked(unsigned int b)
+void *malloc_checked(unsigned int b);
 char *string_nonconcat(char *s1, char *s2, unsigned int n);
 void* _calloc(unsigned int nmemb , unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);

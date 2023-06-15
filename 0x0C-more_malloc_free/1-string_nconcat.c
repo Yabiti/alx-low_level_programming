e5257617f961d40e96710656fd060c473071499b
Yabiti <yab@gmail.com>
2023-06-15 22:20:38 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,51 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
-*string_nonconcat number of bytes to concatenate.
-*@s1: string to append
-*@s2: string to concatenate
-*@n1: number of bytes to concatenate s2 to s1.
-*Return: pointer to allocate a newly space in memory.
+ * string_nconcat - function that concatenates two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of @s2 to add to @s1 to be a new string
+ *
+ * Return: new string followed by the first @n bytes
+ *         of string 2 @s2 or NULL
 */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s;
-unsignedint i = 0; j = 0; len1 = 0; len2 = 0;
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2n2])
-n2++;
-if (n < 2)
-s = malloc(sizeof(char) * (len1 + n + 1));
-else
-s = malloc(sizeof(char) * (len2 + n + 1));
-if (!s)
-return (NULL);
-while (i < n1)
-{
-s[i] = s1[i];
-i++;
-}
-while (n < 2 && i < (len1 + n2))
-s[i++] = s2[j++];
-s[i] = '\0';
-return (s);
+	unsigned int l1, l2, i, j;
+	char *str;
+
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+
+	/*set n to length of s2*/
+	if (n >= l2)
+		n = l2;
+
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }

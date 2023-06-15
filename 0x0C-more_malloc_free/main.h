94fe5cbafe38f6e6bcc77f318f7b5ac18d017baf
Yabiti <yab@gmail.com>
2023-06-15 18:56:59 +0300

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b)
+char *string_nonconcat(char *s1, char *s2, unsigned int n);
+void* _calloc(unsigned int nmemb , unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+#endif

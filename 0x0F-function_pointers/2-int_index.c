e516021b87f4797c8c821bf0d3c6f29493837bbe
Yabiti <yab@gmail.com>
2023-06-21 15:10:08 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,14 +8,14 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
-    if (array == NULL || size <=0 || cmp == NULL)
-    return (-1);
+int i;
+if (array == NULL || size <=0 || cmp == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]))
-        return (0);
-    }
-    return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (0);
+}
+return (-1);
 }

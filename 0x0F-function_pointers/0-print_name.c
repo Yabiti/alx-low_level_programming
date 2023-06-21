fc94e5b31c70d0dceff1b4c1e64bf66c62d584d7
Yabiti <yab@gmail.com>
2023-06-21 10:00:27 +0300

    function_pointers.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "functoin_pointers.h"
+#include <stdio.h>
+/**
+* print_name - print name usingpointer to function
+*@name: string to add
+*@fun: pointer to function
+*Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL || f == NULL)
+    return;
+    f(name);
+}

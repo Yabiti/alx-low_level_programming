69364a67eed5613ffec1af6dbe07d550942f3cc2
Yabiti <yab@gmail.com>
2023-06-21 14:33:21 +0300

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 /**
 * print_name - print name usingpointer to function
 *@name: string to add
-*@fun: pointer to function
+*@f: pointer to function
 *Return: nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 return;
-f(name);
+f(name);  
 }
 

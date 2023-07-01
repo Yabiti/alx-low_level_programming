4c0432405a4d5e5bb17e7ebb65dba95df633792b
Yabiti <yab@gmail.com>
2023-07-01 20:52:28 +0300

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@ void first(void) __attribute__ ((constructor));
 */
 void first(void)
 {
-printf("you're a beat!(and yet, you must allow,\n");
-printf(" i bore my house up on my back!\n");
+printf("you're a beat! and yet, you must allow,\n");
+printf(" i bore my house upon my back!\n");
 }

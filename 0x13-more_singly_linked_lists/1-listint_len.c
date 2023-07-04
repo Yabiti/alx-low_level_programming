dc74eb952b45c0e381b989f36a0e1b582991a95c
Yabiti <yab@gmail.com>
2023-07-04 15:19:18 +0300

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,11 +6,13 @@
 */
 size_t listint_len(const listint_t *h)
 {
-    size_t num = 0;
-    while (h)
-    {
-        num++;
-        h = h->next;
-    }
-    return (num);
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
 }
+return (num);
+}
+
+

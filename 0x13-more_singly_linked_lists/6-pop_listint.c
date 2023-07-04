b3ae42ed02a210d24c2184376e5ec4241d611d35
Yabiti <yab@gmail.com>
2023-07-04 17:00:50 +0300

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,13 +6,13 @@
 */
 int pop_listint(listint_t **head)
 {
-    listint_t *temp;
-    int num;
-    if (!head || !*head)
-    return (0);
-    num = (*head)->n;
-    temp = (*head)->next;
-    free(*head);
-    *head = temp;
-    return (num);
+listint_t *temp;
+int num;
+if (!head || !*head)
+return (0);
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (num);
 }

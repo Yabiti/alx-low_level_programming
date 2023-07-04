e6f26c560715909e1c16233a84a3cf8702d69d3e
Yabiti <yab@gmail.com>
2023-07-04 16:13:56 +0300

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,10 +6,10 @@
 void free_listint(listint_t *head)
 {
     listint_t *temp;
-    while (head)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }

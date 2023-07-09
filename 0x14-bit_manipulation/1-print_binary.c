daae688b65644a9b598b278c6161bc6397e5cdad
Yabiti <yabetseyohannes@gmail.com>
2023-07-09 23:43:35 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 *print_binary - prints the binary representation of a number
 *@n: number to be printed
@@ -12,6 +12,6 @@ void print_binary(unsigned long int n)
 if (n > 1)
 	print_binary(n >> 1);
 
-    putchar((n & 1) + '0');
+_putchar((n & 1) + '0');
 
 }

98f1fc23d64a6490b34d99da86daca280d728af0
Yabiti <yabetseyohannes@gmail.com>
2023-07-11 21:07:44 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void __attribute__((constructor)) hare(void);
 void hare(void)
 {
 printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n")
+"I bore my house upon my back!\n");
 }
+

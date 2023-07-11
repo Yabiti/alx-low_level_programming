39a1d94dfb8b4a385e7115f29215c35d6c794b3c
Yabiti <yabetseyohannes@gmail.com>
2023-07-11 20:33:54 +0300

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -305,9 +305,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_data(header->e_ident);
 	print_version(header->e_ident);
 	print_osabi(header->e_ident;
-	print_abi(header->e_ident)
-	print_type(header->e_type, header->e_ident)
-	print_entry(header->e_entry, header->e_ident)
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);

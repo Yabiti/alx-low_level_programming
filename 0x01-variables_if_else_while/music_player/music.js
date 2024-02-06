64a4ba726b7436efcff24ae08c73f422b427c38d
Yabiti <yabetseyohannes@gmail.com>
2024-02-06 19:41:38 +0300

    .

---
+++ 0x01-variables_if_else_while/music_player/music.js
@@ -0,0 +1,4 @@
+let now_paying = document.querySelector('.now_playing');
+let track_art = document.querySelector('.track_art');
+let track_name = document.querySelector('.track_name');
+let track_artist = document.querySelector('.track_artist');

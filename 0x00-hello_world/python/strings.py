2cf9117506a31d5bc0ecc35c9836b60e3964cbc4
Yabiti <yabetseyohannes@gmail.com>
2024-02-20 15:17:24 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -4,3 +4,7 @@ if score >= 90 and score < 100:
     print("Grade: A")
 if score >= 80 and score < 90:
     print("Grade: B")
+if score >= 70 and score < 80:
+    print("Grade: C")
+if score >= 60 and score < 70:
+    

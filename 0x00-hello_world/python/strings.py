75d4224acf2ab4b7ab8ccc9761a4c18f06a6fe79
Yabiti <yabetseyohannes@gmail.com>
2024-02-20 15:28:19 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,12 +1,12 @@
 score = int(input("score: "))
 
-if score >= 90 and score <= 100:
+if score >= 90:
     print("Grade: A")
-elif score >= 80 and score < 90:
+elif score >= 80:
     print("Grade: B")
-elif score >= 70 and score < 80:
+elif score >= 70:
     print("Grade: C")
-elif score >= 60 and score < 70:
+elif score >= 60:
     print("Grade: D")
 else:
     print("Grade: F")

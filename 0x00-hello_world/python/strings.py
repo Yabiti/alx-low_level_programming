06a3fcb4e32be1ad5855e2218796ac736289308d
Yabiti <yabetseyohannes@gmail.com>
2024-02-20 15:13:51 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,6 +1,2 @@
-name = input("what's your name? ")
-match name:
-    case "Harry" | "Hermione" | "Draco":
-        print("gryffindor")
-    case _:
-        print("Who?")
+score = int(input("score "))
+

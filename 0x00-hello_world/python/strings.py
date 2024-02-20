9e29b75f620b99df56df7065c6d605f15aa52639
Yabiti <yabetseyohannes@gmail.com>
2024-02-20 13:31:51 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -2,3 +2,9 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
+if number > 0:
+    print("the number is positive")
+elif number < 0:
+    print("the number is negative")
+else:
+    print("the number is Zero")

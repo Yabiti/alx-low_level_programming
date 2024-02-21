a50be63f30fc539555e472168144d358bddea022
Yabiti <yabetseyohannes@gmail.com>
2024-02-21 11:22:00 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -2,4 +2,7 @@ def main():
     my_love(get_number())
 
 def get_number():
-    n = int(input("who's she? "))
+    while True:
+        n = int(input("who's she? "))
+        if n > 1:
+            return n

1fc0102bdc2bdfc6effb9652f8c64b014753bfcf
Yabiti <yabetseyohannes@gmail.com>
2024-02-21 11:20:41 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,13 +1,5 @@
 def main():
-    meow(get_number())
-    
+    my_love(get_number())
+
 def get_number():
-    n = int(input("what's n? "))
-    if n > 1:
-        return n
-
-def meow(n):
-    for _ in range(n):
-        print("meow")
-
-main()
+    n = int(input("who's she? "))

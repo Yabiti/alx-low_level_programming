d7a61d8beba4704f9a65a46fa58918bc9b6c5a35
Yabiti <yabetseyohannes@gmail.com>
2024-02-22 13:25:43 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,12 +1,12 @@
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's x?"))
+            return int(input(prompt))
         except ValueError:
             pass
 

63882125211364d2765c85a9d029f76042161500
Yabiti <yabetseyohannes@gmail.com>
2024-02-27 14:48:08 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -2,4 +2,7 @@ def f():
     raise OSError("operation falied")
 excs = []
 for i in range(3):
-    
+    try:
+        f()
+    except Exception as e:
+        e.add_note(f"happened in iteration {i + 1}")

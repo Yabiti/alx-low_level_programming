9333b2c6e58517781deae864fbcb4732e75bfa61
Yabiti <yabetseyohannes@gmail.com>
2024-02-27 14:53:29 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,10 +1,3 @@
-def f():
-    raise OSError("operation falied")
-excs = []
-for i in range(3):
-    try:
-        f()
-    except Exception as e:
-        e.add_note(f"happened in iteration {i + 1}")
-    excs.append(e)
-    raise Exception("we have some problems", excs)
+from random import choice
+
+coin = choice(["head", "tails"])

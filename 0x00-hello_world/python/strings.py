8c32f1565232bc4c33923309383b6edf7f3e68bf
Yabiti <yabetseyohannes@gmail.com>
2024-02-27 13:55:32 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,15 +1,1 @@
-def main():
-    x = get_int()
-    print(f"x is {x}")
-
-def get_int():
-    while True:
-        try:
-            x = int(input("what's x? "))
-        except ValueError:
-            print("x is not an integer")
-        else:
-            break
-        return x
-    
-main()
+raise NameError('hi there')

b4f89f7fce6305b5e301cbbb78165c799c38112d
Yabiti <yabetseyohannes@gmail.com>
2024-02-28 11:12:55 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -4,7 +4,4 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests("https//-------------"+ sys.argv[1])
-print(response.json)
-
-print(json.dumps(response.json(), indent=2))
+response = requests("https//---------------" + sys.qrgv[1])

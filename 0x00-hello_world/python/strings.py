820e8d6874707ed01e34194c2dd177055ce13b1a
Yabiti <yabetseyohannes@gmail.com>
2024-02-28 10:50:15 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,6 +1,6 @@
 import sys
 
 try:
-    print("hello my name is", sys.argv[1])
+    print("do you have some explanations", sys.argv[1])
 except IndexError:
-    print("too few arguments")
+    print("Too few arguments")

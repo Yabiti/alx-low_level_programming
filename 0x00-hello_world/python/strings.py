4c121d07821a7826515ffe5ae4cffd6cf9981141
Yabiti <yabetseyohannes@gmail.com>
2024-02-29 13:33:39 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -2,3 +2,6 @@ import csv
 
 name = input("what's your name? ")
 home = input("wher's your home? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])

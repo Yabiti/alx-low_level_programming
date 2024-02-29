e73ca74347ed481cb65048d9dac61f6a261ad691
Yabiti <yabetseyohannes@gmail.com>
2024-02-29 10:36:55 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -2,4 +2,8 @@ students = []
 
 with open("students.csv") as file:
     for line in file:
-        
+        name , home = line.strip().split(",")
+        students.append(f"{name} is in {home}")
+
+for student in sorted(students):
+    print(student)

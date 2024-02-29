0d99978b43874bb0ac65cd4691d77a594bae2af7
Yabiti <yabetseyohannes@gmail.com>
2024-02-29 10:23:52 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,4 +1,4 @@
 with open("students.csv") as file:
     for line in file:
-        home , name = line.strip().split(",")
-        print(f"['name'] is in ['house']")
+        name , home = line.strip().split(",")
+        print(f"{name} is in {home}")

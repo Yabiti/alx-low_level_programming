35bee1b5b235d1c191f4ac9fb70a8af8d5222583
Yabiti <yabetseyohannes@gmail.com>
2024-02-29 11:34:02 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -2,8 +2,5 @@ students = []
 
 with open("students.csv") as file:
     for line in file:
-        name, house = line.rstrip().split(",")
-        students.append({"name": name, "house": house})
-
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+        name , home = line.strip().split(",")
+        students.append({"name": name , "home": home})

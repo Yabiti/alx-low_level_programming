bbd097082e917e5207ddc16e2dbfb0a63124c53a
Yabiti <yabetseyohannes@gmail.com>
2024-02-29 13:32:17 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,10 +1,4 @@
 import csv
 
-students = []
-
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row['name'], "home": row['home']})
-    for student in sorted(students, key=lambda student: student['name']):
-        print(f"{student['name']}, is in {student['home']}")
+name = input("what's your name? ")
+home = input("wher's your home? ")

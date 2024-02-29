9f9df522188e0cfc57f4852ee15847a2e002ef94
Yabiti <yabetseyohannes@gmail.com>
2024-02-29 12:37:54 +0300

    .

--- 0x00-hello_world/python/strings.py
+++
@@ -1,9 +0,0 @@
-students = []
-
-with open("students.csv") as file:
-    for line in file:
-        name, home = line.strip().split(",")
-        students.append({"name": name, "home": home})
-    
-    for student in sorted(students, key=lambda student: student["name"]):
-        print(f"{student['name']} is always stay at {student['home']}")

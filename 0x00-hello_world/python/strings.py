51d83aba9ba23f3585fd32c1384c3d265252d312
Yabiti <yabetseyohannes@gmail.com>
2024-03-01 13:59:03 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -3,5 +3,5 @@ import csv
 students = []
 with open("students.csv") as file:
     reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row['name'], "home":
+    for row in reader
+        students.append({"name": row['name']

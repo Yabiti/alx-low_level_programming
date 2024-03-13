c9b2137fdf62698c7bcda901aae67b401d4cf7f0
Yabiti <yabetseyohannes@gmail.com>
2024-03-13 18:00:15 +0300

    package

--- 0x02-ES6_classes/0-main.js
+++
@@ -1,9 +0,0 @@
-import ClassRoom from "./0-classroom.js";
-
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 0-main.js 
-10
-bob@dylan:~$

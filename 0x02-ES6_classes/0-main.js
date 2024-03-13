8d4e6fb8d0f08acdbc660b6c3981ce64b0140281
Yabiti <yabetseyohannes@gmail.com>
2024-03-13 17:51:49 +0300

    .

---
+++ 0x02-ES6_classes/0-main.js
@@ -0,0 +1,9 @@
+import ClassRoom from "./0-classroom.js";
+
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize)
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 0-main.js 
+10
+bob@dylan:~$

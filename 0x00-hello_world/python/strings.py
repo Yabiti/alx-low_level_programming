ec6314deaa818c0d53a415ff725995f35c183c3a
Yabiti <yabetseyohannes@gmail.com>
2024-03-14 11:36:02 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,6 +1,8 @@
 email = input("what's our email ").strip()
 
-if "@" in email and "." in email:
+username, domain = email.split("@")
+
+if username and domain.endswith(".edu"):
     print("valid")
 else:
     print("invalid")

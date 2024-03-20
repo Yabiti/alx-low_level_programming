6dbf286307198fe38176a3470d5e04a6378b1aca
Yabiti <yabetseyohannes@gmail.com>
2024-03-20 22:17:59 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,9 +1,5 @@
 #Ask user for their name
-name = input("what's your name? ")
+name = input("what's your name? ").strip().title()
 
-#removing white space
-name = name.strip()
-#capitalizing user name
-name = name.title()
 #say hello to the user
 print(f"Hello, {name}")

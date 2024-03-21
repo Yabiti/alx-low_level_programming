7f85d050dce163dde7dd20724e4dd663ce31685e
Yabiti <yabetseyohannes@gmail.com>
2024-03-21 12:54:20 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,8 +1,3 @@
-def main():
-    x = int(input("what's x? "))
-    print("x squared is ", square(x))
-
-def square(n):
-    return pow(n, 2)
-
-main()
+value = input('this/is/cs50')
+value.hex('-')
+print(value)

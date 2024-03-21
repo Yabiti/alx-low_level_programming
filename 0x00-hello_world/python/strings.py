d29d85959b69532579a667848baee630bf5f9ee0
Yabiti <yabetseyohannes@gmail.com>
2024-03-21 09:54:38 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,5 +1,6 @@
-x = float(input("what's x? "))
-y = float(input("what's y? "))
+def hello():
+    hello(name)
 
-z = x / y
-print(f"{z:.2f}")
+name = input("what's your  name? ")
+hello()
+print(name)

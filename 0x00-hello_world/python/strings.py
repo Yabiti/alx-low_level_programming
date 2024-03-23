aa7a389c6ce44c6d7df7894dca44fad393cc0c79
Yabiti <yabetseyohannes@gmail.com>
2024-03-23 13:01:17 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,2 +1,7 @@
 txt = "my name is {fname}, i'm {age}".format(fname="john", age=35)
+txt1 = "my name is {0} , im {1}".format('lewa', 35)
+txt2 = "my name is {}, i'm {}".format("yabetse", 25)
+
 print(txt)
+print(txt1)
+print(txt2)

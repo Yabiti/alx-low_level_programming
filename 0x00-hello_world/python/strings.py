25c62f1f9c0e7461051de23ac6f83f54326e72e6
Yabiti <yabetseyohannes@gmail.com>
2024-03-26 12:12:26 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,4 +1,18 @@
-txt = "50"
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
 
-x = txt.zfill(10)
-print(x)
+
+def dollars_to_float(d):
+    d_without_dollar_sign = d.replace("$", "")
+    return float(d_without_dollar_sign)
+
+
+def percent_to_float(p):
+    p_without_percentage = p.replace("%", "")
+    return float(p_without_percentage)
+
+
+main()

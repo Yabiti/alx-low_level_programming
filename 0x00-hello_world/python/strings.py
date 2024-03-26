b6659d86bf9cf1c2193d576e35f34ebfd3ba7678
Yabiti <yabetseyohannes@gmail.com>
2024-03-26 13:05:26 +0300

    .

--- 0x00-hello_world/python/strings.py
+++ 0x00-hello_world/python/strings.py
@@ -1,18 +1,9 @@
-def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
+user = input("what's the universe answer? ")
 
-
-def dollars_to_float(d):
-    d_without_dollar_sign = d.replace("$", "")
-    return float(d_without_dollar_sign)
-
-
-def percent_to_float(p):
-    p_without_percentage = p.replace("%", "")
-    return float(p_without_percentage)
-
-
-main()
+if user == "42":
+    print("yes")
+elif user == "forty two":
+    print("yes")
+elif user == "forty-two":
+    print("yes")
+else:
